aa5ad3fdcd24533e2c7b6c0bf8eb25abd952e770
Annette Odingo <odingoannette@gmail.com>
2020-06-03 13:36:04 +0100

    Corrected player location

--- src/adv.py
+++ src/adv.py
@@ -1,3 +1,4 @@
+import textwrap
 from room import Room
 from player import Player
 # Declare all the rooms
@@ -38,13 +39,14 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player("outside")
+player = Player(room['outside'])
 
 # Write a loop that:
 #
 # * Prints the current room name
-print(player)
+print(player.location)
 # * Prints the current description (the textwrap module might be useful here).
+
 # * Waits for user input and decides what to do.
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
